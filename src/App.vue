b5a3fd513e88f0e08822ae3e97a1d110c227679c
Inusah-Said <saidinusah29@gmail.com>
2022-02-26 23:06:47 +0000

    routingn added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
-import AuthView from "../src/views/AuthView.vue";
 </script>
 
 <template>
-  <div>
-    <AuthView />
-  </div>
+
+    <router-view></router-view>
+
 </template>
 <style>
 .signIn {
