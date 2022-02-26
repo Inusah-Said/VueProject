bdd573936b3eb8b6bef508e9941997b179ad6697
Inusah-Said <saidinusah29@gmail.com>
2022-02-26 22:46:42 +0000

    working on routing

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,16 +1,112 @@
-<script></script>
+<script setup>
+import AuthViewContainer from '../components/containers/AuthViewContainer.vue';
+import { ref } from "vue";
+
+let showPassword = ref(false);
+const toggleShowPassword = (evt) => {
+  evt.preventDefault();
+  showPassword.value = !showPassword.value;
+};
+</script>
 <template>
-  <div
-    class="w-screen h-screen flex items-center justify-center auth-container"
-  >
-    <div class="bg-white px-10">
-      <slot> </slot>
+<AuthViewContainer>
+  <div class="space-y-5">
+      <h4 class="w-full text-center text-5xl font-caligra py-2 text-appBlack">
+        Todolist
+      </h4>
+      <button
+        class="w-full flex items-center justify-center text-2xl font-light space-x-5 border border-[#677ACB] py-2 px-5 rounded-md"
+      >
+        <img src="../assets/assets/googleIcon.png" class="w-6 h-6" />
+        <p class="text-appBlack text-lg font-light font-vernir">
+          Continue with Google
+        </p>
+      </button>
+      <img
+        class="signIn"
+        src="../assets/assets/signInEmail.svg"
+        alt="sign in with email"
+      />
+
+      <form class="w-full">
+        <label class="block space-y-1">
+          <span class="block font-medium text-appBlack font-vernir">
+            Email
+          </span>
+          <input
+            type="email"
+            placeholder="mary@example.com"
+            class="peer w-full focus:outline-appPurple border shadow-sm border-slate-300 py-2 px-3 rounded-md"
+          />
+          <p
+            class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm font-vernir"
+          >
+            Please provide a valid email address.
+          </p>
+        </label>
+        <label class="block space-y-1">
+          <span class="block font-medium text-appBlack font-vernir">
+            Password
+          </span>
+          <div class="relative w-full">
+            <input
+              :type="showPassword === true ? 'text' : 'password'"
+              placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+              class="peer w-full outline-[#29325A33] bg-inherit focus:outline-appPurple border shadow-sm border-slate-300 h-10 px-3 rounded-md"
+            />
+            <div class="absolute right-1 top-1">
+              <button
+                @click='toggleShowPassword'
+                class="h-full hover:bg-transparent normal-case"
+              >
+                <img
+                  v-if="showPassword"
+                  src="../assets/assets/visibleEye.svg"
+                  class="w-5 h-5 r-2"
+                />
+                <img
+                  v-else
+                  src="../assets/assets/hidePassword.svg"
+                  class="w-5 h-5 r-2"
+                />
+              </button>
+            </div>
+          </div>
+        </label>
+        <div class="flex justify-between items-center pb-4 pt-2">
+          <div class="flex items-center justify-center space-x-2">
+            <input type="checkbox" />
+            <p class="text-xs text-appBlack font-vernir">Remember me</p>
+          </div>
+
+          <p class="text-xs text-appPurple font-vernir">Forgot Password?</p>
+        </div>
+
+        <button
+          type="submit"
+          class="bg-appPurple w-full py-2 my-3 text-white rounded-md font-vernir"
+        >
+          Login
+        </button>
+        <p class="text-xs text-center font-medium text-appBlack font-vernir">
+          Not registered yet?
+          <span class="text-xs space-x-2 text-appPurple font-medium">
+            Create account
+          </span>
+        </p>
+      </form>
     </div>
-  </div>
+</AuthViewContainer>
 </template>
 
 <style>
-.auth-container {
-  background: url("./src/assets/assets/authBg.svg");
+	.auth-container {
+		background: url('./src/assets/assets/authBg.svg');
+	}
+
+.signIn {
+  width: 400px;
+  height: 30px;
 }
 </style>
+
