2b3a07a22254aadfe1977dce2407fd5c4cf35e5f
Inusah-Said <saidinusah29@gmail.com>
2022-02-26 22:04:29 +0000

    added tailwind config, added th app vue screen

---
+++ src/views/AuthView.vue
@@ -0,0 +1,16 @@
+<script></script>
+<template>
+  <div
+    class="w-screen h-screen flex items-center justify-center auth-container"
+  >
+    <div class="bg-white px-10">
+      <slot> </slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.auth-container {
+  background: url("./src/assets/assets/authBg.svg");
+}
+</style>
