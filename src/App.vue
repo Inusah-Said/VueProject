910ebbab163f7c05ffb12972a2031f93560a6ed2
Inusah-Said <saidinusah29@gmail.com>
2022-02-26 22:09:33 +0000

    add fixes

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,107 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "@/components/HelloWorld.vue";
+	import AuthView from '@/views/AuthView.vue';
+	import { ref } from 'vue';
+
+	let showPassword = ref(false);
+	const toggleShowPassword = (evt) => {
+		evt.preventDefault();
+		showPassword.value = !showPassword.value;
+	};
 </script>
 
 <template>
-<!--  <RouterView />-->
-  <div>
-    <p>This is my app</p>
-  </div>
+	<AuthView>
+		<div class="space-y-5">
+			<h4 class="w-full text-center text-5xl font-caligra py-2 text-appBlack">
+				Todolist
+			</h4>
+			<button
+				class="w-full flex items-center justify-center text-2xl font-light space-x-5 border border-[#677ACB] py-2 px-5 rounded-md"
+			>
+				<img src="./assets/assets/googleIcon.png" class="w-6 h-6" />
+				<p class="text-appBlack text-lg font-light font-vernir">
+					Continue with Google
+				</p>
+			</button>
+			<img
+				class="signIn"
+				src="./assets/assets/signInEmail.svg"
+				alt="sign in with email"
+			/>
+
+			<form class="w-full">
+				<label class="block space-y-1">
+					<span class="block font-medium text-appBlack font-vernir">
+						Email
+					</span>
+					<input
+						type="email"
+						placeholder="mary@example.com"
+						class="peer w-full focus:outline-appPurple border shadow-sm border-slate-300 py-2 px-3 rounded-md"
+					/>
+					<p
+						class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm font-vernir"
+					>
+						Please provide a valid email address.
+					</p>
+				</label>
+				<label class="block space-y-1">
+					<span class="block font-medium text-appBlack font-vernir">
+						Password
+					</span>
+					<div class="relative w-full">
+						<input
+							:type="showPassword === true ? 'text' : 'password'"
+							placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
+							class="peer w-full outline-[#29325A33] bg-inherit focus:outline-appPurple border shadow-sm border-slate-300 h-10 px-3 rounded-md"
+						/>
+						<div class="absolute right-1 top-1">
+							<button
+								@click="toggleShowPassword"
+								class="h-full hover:bg-transparent normal-case"
+							>
+								<img
+									v-if="showPassword"
+									src="./assets/assets/visibleEye.svg"
+									class="w-5 h-5 r-2"
+								/>
+								<img
+									v-else="showPassword"
+									src="./assets/assets/hidePassword.svg"
+									class="w-5 h-5 r-2"
+								/>
+							</button>
+						</div>
+					</div>
+				</label>
+				<div class="flex justify-between items-center pb-4 pt-2">
+					<div class="flex items-center justify-center space-x-2">
+						<input type="checkbox" />
+						<p class="text-xs text-appBlack font-vernir">Remember me</p>
+					</div>
+
+					<p class="text-xs text-appPurple font-vernir">Forgot Password?</p>
+				</div>
+
+				<button
+					type="submit"
+					class="bg-appPurple w-full py-2 my-3 text-white rounded-md font-vernir"
+				>
+					Login
+				</button>
+				<p class="text-xs text-center font-medium text-appBlack font-vernir">
+					Not registered yet?
+					<span class="text-xs space-x-2 text-appPurple font-medium">
+						Create account
+					</span>
+				</p>
+			</form>
+		</div>
+	</AuthView>
 </template>
-
 <style>
-@import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
+	.signIn {
+		width: 400px;
+		height: 30px;
+	}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
-  content: [],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        appBlack: "#29325A",
+        appPurple: "#677ACB",
+        appSeaBlue: "#6FC9D9",
+        applightGray: "#29325A33",
+        appDeepGray: "#29325A66",
+        appAlmostWhite: "#F2F5FF",
+        selectedItemColor: "#29325A66",
+        medium: "#CB9134",
+        appHigh: "#B93909",
+        appProgress: "#677ACB66",
+        appHouseKeeping: "#CB916733",
+        completed: "#F4FFF2",
+        money: "#67CBAC33",
+        low: "#6FC9D9",
+        appOverdue: "#EFCACA",
+        appHouse: "#CB916733",
+        family: "#E58D8D33",
+        education: "#8A904833",
+        work: "#A867CB33",
+        travel: "#7367CB33",
+      },
+      fontFamily: {
+        caligra: ["Caligature Hunters", "caliga"],
+        vernir: ["Avenir", "vernir"],
+      },
+    },
   },
   plugins: [],
-}
+};
