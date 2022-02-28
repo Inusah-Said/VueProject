4c22a992adb7bf23dfb36c5f7cec54286141e871
Inusah-Said <saidinusah29@gmail.com>
2022-02-28 07:55:51 +0000

    hahah

--- src/components/containers/AuthViewContainer.vue
+++ src/components/containers/AuthViewContainer.vue
@@ -1,18 +1,16 @@
-<script>
-
-</script>
+<script></script>
 <template>
-	<div
-		class="w-screen h-screen flex items-center justify-center auth-container"
-	>
-		<div class="bg-white px-10">
-			<slot> </slot>
-		</div>
-	</div>
+  <div
+    class="w-screen h-screen flex items-center justify-center auth-container"
+  >
+    <div class="bg-white px-10">
+      <slot> </slot>
+    </div>
+  </div>
 </template>
 
 <style>
-	.auth-container {
-		background: url('./src/assets/assets/authBg.svg');
-	}
+.auth-container {
+  background: url("./src/assets/assets/authBg.svg");
+}
 </style>
