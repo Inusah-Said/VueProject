202d352bc74ff53f21114b836631c5df572b5b65
Inusah-Said <saidinusah29@gmail.com>
2022-02-28 07:51:45 +0000

    worked on auth, added tailwind and kutty

--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,6 +1,7 @@
 <script setup>
-import AuthViewContainer from '../components/containers/AuthViewContainer.vue';
+import AuthViewContainer from "../components/containers/AuthViewContainer.vue";
 import { ref } from "vue";
+import googleIcon from "../assets/assets/googleIcon.png";
 
 let showPassword = ref(false);
 const toggleShowPassword = (evt) => {
@@ -9,18 +10,18 @@ const toggleShowPassword = (evt) => {
 };
 </script>
 <template>
-<AuthViewContainer>
-  <div class="space-y-5">
+  <AuthViewContainer>
+    <div class="space-y-5">
       <h4 class="w-full text-center text-5xl font-caligra py-2 text-appBlack">
         Todolist
       </h4>
       <button
         class="w-full flex items-center justify-center text-2xl font-light space-x-5 border border-[#677ACB] py-2 px-5 rounded-md"
       >
-        <img src="../assets/assets/googleIcon.png" class="w-6 h-6" />
-        <p class="text-appBlack text-lg font-light font-vernir">
+        <img :src="googleIcon" class="w-6 h-6" alt="google logo" />
+        <span class="text-appBlack text-lg font-light font-vernir">
           Continue with Google
-        </p>
+        </span>
       </button>
       <img
         class="signIn"
@@ -38,40 +39,42 @@ const toggleShowPassword = (evt) => {
             placeholder="mary@example.com"
             class="peer w-full focus:outline-appPurple border shadow-sm border-slate-300 py-2 px-3 rounded-md"
           />
-          <p
+          <span
             class="mt-2 invisible peer-invalid:visible text-pink-600 text-sm font-vernir"
           >
             Please provide a valid email address.
-          </p>
+          </span>
         </label>
         <label class="block space-y-1">
           <span class="block font-medium text-appBlack font-vernir">
             Password
           </span>
-          <div class="relative w-full">
+          <span class="relative w-full">
             <input
               :type="showPassword === true ? 'text' : 'password'"
               placeholder="&bull;&bull;&bull;&bull;&bull;&bull;&bull;&bull;"
               class="peer w-full outline-[#29325A33] bg-inherit focus:outline-appPurple border shadow-sm border-slate-300 h-10 px-3 rounded-md"
             />
-            <div class="absolute right-1 top-1">
+            <span class="absolute right-1 top-1">
               <button
-                @click='toggleShowPassword'
+                @click="toggleShowPassword"
                 class="h-full hover:bg-transparent normal-case"
               >
                 <img
                   v-if="showPassword"
                   src="../assets/assets/visibleEye.svg"
                   class="w-5 h-5 r-2"
+                  alt="show password as text"
                 />
                 <img
                   v-else
                   src="../assets/assets/hidePassword.svg"
                   class="w-5 h-5 r-2"
+                  alt="show password as hashed"
                 />
               </button>
-            </div>
-          </div>
+            </span>
+          </span>
         </label>
         <div class="flex justify-between items-center pb-4 pt-2">
           <div class="flex items-center justify-center space-x-2">
@@ -96,17 +99,12 @@ const toggleShowPassword = (evt) => {
         </p>
       </form>
     </div>
-</AuthViewContainer>
+  </AuthViewContainer>
 </template>
 
 <style>
-	.auth-container {
-		background: url('./src/assets/assets/authBg.svg');
-	}
-
 .signIn {
   width: 400px;
   height: 30px;
 }
 </style>
-
