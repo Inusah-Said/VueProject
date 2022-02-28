88bc70a86ba70777b52c630218d9a07ce432561a
Inusah-Said <saidinusah29@gmail.com>
2022-02-28 10:46:52 +0000

    added extra screens, some layout, calendar,tasks screens

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="fixed top-0 left-0 bg-red-200 w-full pl-[220px] h-10">
+    <div class="flex justify-between container mx-auto px-4 py-2">
+      <div class="flex space-x-4">
+        <h1>Header</h1>
+
+        <input placeholder="Search anything" class="bg-appAlmostWhite" />
+      </div>
+      <h5>Said</h5>
+    </div>
+  </nav>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ src/views/TasksView.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>this is the tasks screen</h1>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
